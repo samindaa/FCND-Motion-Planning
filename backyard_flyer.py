3b6d71a1a258a4b8874a5c676dadbccaef03e4b7
Saminda Abeyruwan <saminda@saminda.local>
2018-04-21 17:21:35 -0700

    helper file updates

---
+++ backyard_flyer.py
@@ -0,0 +1,219 @@
+import argparse
+import time
+from enum import Enum
+
+import numpy as np
+
+from udacidrone import Drone
+from udacidrone.connection import MavlinkConnection, WebSocketConnection  # noqa: F401
+from udacidrone.messaging import MsgID
+
+
+class States(Enum):
+    MANUAL = 0
+    ARMING = 1
+    TAKEOFF = 2
+    WAYPOINT = 3
+    PRE_LANDING = 4  # Added a new State
+    LANDING = 5
+    DISARMING = 6
+
+
+class BackyardFlyer(Drone):
+
+    def __init__(self, connection):
+        super().__init__(connection)
+        self.target_position = np.array([0.0, 0.0, 0.0])
+        self.all_waypoints = []
+        self.idx_waypoint = 0
+        self.target_altitude = 5.0
+        self.in_mission = True
+        self.check_state = {}
+
+        # initial state
+        self.flight_state = States.MANUAL
+
+        # Callbacks
+        self.register_callback(MsgID.LOCAL_POSITION, self.local_position_callback)
+        self.register_callback(MsgID.LOCAL_VELOCITY, self.velocity_callback)
+        self.register_callback(MsgID.STATE, self.state_callback)
+
+    def local_position_callback(self):
+        """
+        TODO: Implement this method
+
+        This triggers when `MsgID.LOCAL_POSITION` is received and self.local_position contains new data
+        """
+        print("local_position_callback")
+        print("wpt: {} state: {}".format(self.idx_waypoint, self.flight_state))
+        print("lpos: {}".format(self.local_position))
+        if self.flight_state == States.TAKEOFF:
+            if -self.local_position[-1] > self.target_position[-1] * 0.95:
+                self.flight_state = States.WAYPOINT
+                self.waypoint_transition()
+        elif self.flight_state == States.WAYPOINT:
+            if np.linalg.norm(self.local_position[0:2] - self.target_position[0:2]) < 1.0:
+                self.waypoint_transition()
+            # Since the project needs to disarm the drone after one cycle, we execute the following code.
+            # Uncommenting the following 2-lines will cause the drone to fly forever!
+            if self.idx_waypoint == 0:
+                self.flight_state = States.PRE_LANDING
+        elif self.flight_state == States.LANDING:
+            if np.abs(self.global_position[-1] - self.global_home[-1]) < 0.1:
+                if np.abs(self.local_position[-1]) < 0.1:
+                    self.disarming_transition()
+
+    def velocity_callback(self):
+        """
+        TODO: Implement this method
+
+        This triggers when `MsgID.LOCAL_VELOCITY` is received and self.local_velocity contains new data
+        """
+        print("velocity_callback")
+        print("wpt: {} state: {}".format(self.idx_waypoint, self.flight_state))
+        if self.flight_state == States.PRE_LANDING and np.linalg.norm(self.local_velocity[0:2]) < 0.2:
+            self.landing_transition()
+
+    def state_callback(self):
+        """
+        TODO: Implement this method
+
+        This triggers when `MsgID.STATE` is received and self.armed and self.guided contain new data
+        """
+        print("state_callback")
+        print("wpt: {} state: {}".format(self.idx_waypoint, self.flight_state))
+        if not self.in_mission:
+            return
+        if self.flight_state == States.MANUAL:
+            self.calculate_box()
+            self.arming_transition()
+        elif self.flight_state == States.ARMING:
+            if self.armed:
+                self.takeoff_transition()
+        elif self.flight_state == States.DISARMING:
+            if not self.armed and not self.guided:
+                self.manual_transition()
+
+    def calculate_box(self):
+        """TODO: Fill out this method
+        
+        1. Return waypoints to fly a box
+        """
+        self.all_waypoints = [[10, 0, self.target_altitude],
+                              [10, 10, self.target_altitude],
+                              [0, 10, self.target_altitude],
+                              [0, 0, self.target_altitude]]
+
+    def arming_transition(self):
+        """TODO: Fill out this method
+        
+        1. Take control of the drone
+        2. Pass an arming command
+        3. Set the home location to current position
+        4. Transition to the ARMING state
+        """
+        print("arming_transition")
+        print("wpt: {} state: {} gpos: {}".format(self.idx_waypoint, self.flight_state, self.global_position))
+
+        self.take_control()
+        self.arm()
+        self.set_home_position(self.global_position[0],
+                               self.global_position[1],
+                               self.global_position[2])
+        self.flight_state = States.ARMING
+
+    def takeoff_transition(self):
+        """TODO: Fill out this method
+        
+        1. Set target_position altitude to 3.0m
+        2. Command a takeoff to 3.0m
+        3. Transition to the TAKEOFF state
+        """
+        print("takeoff_transition")
+        print("wpt: {} state: {}".format(self.idx_waypoint, self.flight_state))
+        self.target_position[-1] = self.target_altitude
+        self.takeoff(self.target_altitude)
+        self.flight_state = States.TAKEOFF
+
+    def waypoint_transition(self):
+        """TODO: Fill out this method
+    
+        1. Command the next waypoint position
+        2. Transition to WAYPOINT state
+        """
+        print("waypoint_transition")
+        print("wpt: {} state: {}".format(self.idx_waypoint, self.flight_state))
+        self.target_position = self.all_waypoints[self.idx_waypoint]
+        print("wpt: {} target: {} altitude: {}".format(self.idx_waypoint,
+                                                       self.target_position,
+                                                       self.target_altitude))
+        # Next waypoint.
+        self.idx_waypoint = (self.idx_waypoint + 1) % len(self.all_waypoints)
+        self.cmd_position(self.target_position[0],
+                          self.target_position[1],
+                          self.target_altitude,
+                          0)
+
+    def landing_transition(self):
+        """TODO: Fill out this method
+        
+        1. Command the drone to land
+        2. Transition to the LANDING state
+        """
+        print("landing_transition")
+        print("wpt: {} state: {}".format(self.idx_waypoint, self.flight_state))
+        self.land()
+        self.flight_state = States.LANDING
+
+    def disarming_transition(self):
+        """TODO: Fill out this method
+        
+        1. Command the drone to disarm
+        2. Transition to the DISARMING state
+        """
+        print("disarm_transition")
+        print("wpt: {} state: {}".format(self.idx_waypoint, self.flight_state))
+        self.disarm()
+        self.flight_state = States.DISARMING
+
+    def manual_transition(self):
+        """This method is provided
+        
+        1. Release control of the drone
+        2. Stop the connection (and telemetry log)
+        3. End the mission
+        4. Transition to the MANUAL state
+        """
+        print("manual_transition")
+        self.release_control()
+        self.stop()
+        self.in_mission = False
+        self.flight_state = States.MANUAL
+        print("wpt: {} state: {}".format(self.idx_waypoint, self.flight_state))
+
+    def start(self):
+        """This method is provided
+        
+        1. Open a log file
+        2. Start the drone connection
+        3. Close the log file
+        """
+        print("Creating log file")
+        self.start_log("Logs", "NavLog.txt")
+        print("starting connection")
+        self.connection.start()
+        print("Closing log file")
+        self.stop_log()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', type=int, default=5760, help='Port number')
+    parser.add_argument('--host', type=str, default='127.0.0.1', help="host address, i.e. '127.0.0.1'")
+    args = parser.parse_args()
+
+    conn = MavlinkConnection('tcp:{0}:{1}'.format(args.host, args.port), threaded=False, PX4=False)
+    # conn = WebSocketConnection('ws://{0}:{1}'.format(args.host, args.port))
+    drone = BackyardFlyer(conn)
+    time.sleep(2)
+    drone.start()
---
+++ sampling.py
@@ -0,0 +1,151 @@
+import numpy as np
+from sklearn.neighbors import KDTree
+from shapely.geometry import Polygon, Point
+
+
+class Poly:
+
+    def __init__(self, coords, height):
+        self._polygon = Polygon(coords)
+        self._height = height
+
+    @property
+    def height(self):
+        return self._height
+
+    @property
+    def coords(self):
+        return list(self._polygon.exterior.coords)[:-1]
+
+    @property
+    def area(self):
+        return self._polygon.area
+
+    @property
+    def center(self):
+        return (self._polygon.centroid.x, self._polygon.centroid.y)
+
+    def contains(self, point):
+        point = Point(point)
+        return self._polygon.contains(point)
+
+    def crosses(self, other):
+        return self._polygon.crosses(other)
+
+
+class Sampler:
+
+    def __init__(self, data, zmax=10, safe_distance=0):
+        self._polygons = self.extract_polygons(data, safe_distance)
+        self._xmin = np.min(data[:, 0] - data[:, 3])
+        self._xmax = np.max(data[:, 0] + data[:, 3])
+
+        self._ymin = np.min(data[:, 1] - data[:, 4])
+        self._ymax = np.max(data[:, 1] + data[:, 4])
+
+        self._zmin = 0
+        # limit z-axis
+        self._zmax = zmax
+        # Record maximum polygon dimension in the xy plane
+        # multiply by 2 since given sizes are half widths
+        # This is still rather clunky but will allow us to 
+        # cut down the number of polygons we compare with by a lot.
+        self._max_poly_xy = 2 * np.max((data[:, 3], data[:, 4]))
+        centers = np.array([p.center for p in self._polygons])
+        self._tree = KDTree(centers, metric='euclidean')
+
+    def extract_polygons(self, data, safe_distance):
+
+        polygons = []
+        for i in range(data.shape[0]):
+            north, east, alt, d_north, d_east, d_alt = data[i, :]
+
+            obstacle = [north - d_north - safe_distance,
+                        north + d_north + safe_distance,
+                        east - d_east - safe_distance,
+                        east + d_east + safe_distance]
+            corners = [(obstacle[0], obstacle[2]), (obstacle[0], obstacle[3]), (obstacle[1], obstacle[3]),
+                       (obstacle[1], obstacle[2])]
+
+            # TODO: Compute the height of the polygon
+            height = alt + d_alt
+
+            p = Poly(corners, height)
+            polygons.append(p)
+
+        return polygons
+
+    # def sample(self, num_samples):
+    #     """Implemented with a k-d tree for efficiency."""
+    #     xvals = np.random.uniform(self._xmin, self._xmax, num_samples)
+    #     yvals = np.random.uniform(self._ymin, self._ymax, num_samples)
+    #     zvals = np.random.uniform(self._zmin, self._zmax, num_samples)
+    #     samples = list(zip(xvals, yvals, zvals))
+    #
+    #     pts = []
+    #     for s in samples:
+    #         in_collision = False
+    #         idxs = list(self._tree.query_radius(np.array([s[0], s[1]]).reshape(1, -1), r=self._max_poly_xy)[0])
+    #         if len(idxs) > 0:
+    #             for ind in idxs:
+    #                 p = self._polygons[int(ind)]
+    #                 if p.contains(s) and p.height >= s[2]:
+    #                     in_collision = True
+    #         if not in_collision:
+    #             pts.append(s)
+    #
+    #     return pts
+
+    def norm(self, x1, x2):
+        return np.linalg.norm(np.array([x1[0], x1[1]]) - np.array([x2[0], x2[1]]))
+
+    def samples(self, num_samples):
+        pts = []
+        while len(pts) < num_samples:
+            in_free_space = False
+            while not in_free_space:
+                s = self.sample()
+                idxs = self._tree.query([[s[0], s[1]]], k=3, return_distance=False)[0]
+                if len(idxs) > 0:
+                    in_collision = False
+                    for ind in idxs:
+                        p = self._polygons[int(ind)]
+                        if p.contains(s) and p.height >= s[2]:
+                            in_collision = True
+                    if not in_collision:
+                        pts.append(s)
+                        in_free_space = True
+        return pts
+
+    def sample(self):
+        xval = np.random.uniform(self._xmin, self._xmax, 1)[0]
+        yval = np.random.uniform(self._ymin, self._ymax, 1)[0]
+        return [xval, yval, self._zmax]
+
+    @property
+    def zmax(self):
+        return self._zmax
+
+    @property
+    def nmin(self):
+        return self._xmin
+
+    @property
+    def emin(self):
+        return self._ymin
+
+    @property
+    def nmax(self):
+        return self._xmax
+
+    @property
+    def emax(self):
+        return self._ymax
+
+    @property
+    def tree(self):
+        return self._tree
+
+    @property
+    def polygons(self):
+        return self._polygons
